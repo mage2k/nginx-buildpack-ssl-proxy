1f32dc5a150f38a29cf7d9bf916f1c43af6c539e
Erik Jones <mage2k@gmail.com>
2018-01-23 16:43:33 -0800

    cc

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -48,7 +48,7 @@ echo "Downloading $open_ssl_url"
 
 (
 	cd nginx-${NGINX_VERSION}
-	./configure \
+	CC=gcc ./configure \
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION} \
